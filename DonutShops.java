5cb8b1aee318375c89194dbd413ff99a1c8eb12a
mihir <mihirpaghdal@gmail.com>
2022-11-11 20:38:58 +0530

    1373A - Donut Shops and 118B - Present from Lena

---
+++ DonutShops.java
@@ -0,0 +1,95 @@
+package CodeForces_Solution;
+
+import java.util.*;
+import java.io.*;
+
+public class DonutShops {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            FastReader in = new FastReader();
+            FastWriter out = new FastWriter();
+            int t = in.nextInt();
+            while(t-->0){
+                long a = in.nextLong();
+                long b = in.nextLong();
+                long c = in.nextLong();
+                long s = -1;
+                long f = -1;
+                if(a<c){
+                    f = 1;
+                }
+                if(a*b > c){
+                    s = b;
+                }
+                out.println(f + " " + s);
+            }
+            out.close();
+        } catch (Exception e) {
+            return;
+        }
+
+    }
+}
