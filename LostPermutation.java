486b7f22fc1db9b177f788d48144b801ea627fea
mihir <mihirpaghdal@gmail.com>
2022-11-19 01:11:27 +0530

    A - Yes-Yes? and B - Lost Permutation added

---
+++ LostPermutation.java
@@ -0,0 +1,104 @@
+package CodeForces_Solution;
+
+import java.util.*;
+import java.io.*;
+
+public class LostPermutation {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            FastReader in = new FastReader();
+            FastWriter out = new FastWriter();
+            int t = in.nextInt();
+            while (t-- > 0) {
+                int n = in.nextInt();
+                int sum = in.nextInt();
+                int curr = 0;
+                int[] arr = new int[n];
+                int m = 0;
+                for(int i = 0; i<n; i++){
+                    int a = in.nextInt();
+                    curr += a;
+                    m = Math.max(m,a);
+                }
+                int currSum = (m*(m+1)/2);
+                int missing = currSum - curr;
+                while(missing<sum){
+                    m++;
+                    missing += m;
+                }
+                if(missing == sum){
+                    out.println("YES");
+                }else{
+                    out.println("NO");
+                }
+            }
+            out.close();
+        } catch (Exception e) {
+            return;
+        }
+
+    }
+}
